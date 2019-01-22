8dbf545c948756937e718464faaac4ec726d8d59
Youth Inno Lab <youthinnolab@gmail.com>
2019-01-21 23:04:13 -0500

    Create questions.js

---
+++ lambda/custom/questions.js
@@ -0,0 +1,66 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      'What is the English translation of Beijing?': [
+        'Eastern Capital',
+        'Southern Capital',
+        'Western Capital',
+        'Northern Capital',
+      ],
+    },
+    {
+      'The palace of the Chinese emperors in Beijing was known by what name? ': [
+        'The Heavenly Palace',
+        'The Tranquil City',
+        'The Empire City',
+        'The Forbidden City',
+      ],
+    },
+    {
+      'A popular pastime among Chinese people is Tai chi, or Tai Ji Quan. What is it?': [
+        'Slow-motion martial art',
+        'A board game similar to chess',
+        'Breathing exercises',
+        'A form of martial arts which uses sticks',
+      ],
+    },
+    {
+      'What is the longest river in China? ': [
+        'Yellow River',
+        'Chang River',
+        'Black Dragon River',
+        'Pearl River',
+      ],
+    },
+    {
+      'How Chinese call their country, and which means what?': [
+        'Central Country',
+        'Last Country',
+        'First Country',
+        'Important Country',
+      ],
+    },
+    {
+      'Moon cakes are associated with what Chinese festival?': [
+        'Chinese New Year',
+        'Dragon Boat Festival',
+        'Mid-Autumn Festival',
+        'Lantern Festival',
+      ],
+    },
+    {
+      'Although it is the Chinese capital, Beijing is not China's largest city. Which Chinese city outstrips Beijing in terms of population?': [
+        'Hong Kong',
+        'Nanjing',
+        'Guangzhou',
+        'Shanghai',
+      ],
+    },
+  ],
+};
